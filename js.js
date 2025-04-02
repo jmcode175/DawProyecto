cb9a9631ef3010ae9bbbe7030337551b898e8a55
codecat <codecatcoding@gmail.com>
2025-04-02 11:10:53 +0100

    Inicialización del proyecto: Agregados los archivos principales index.html, js.js y style.css.

---
+++ js.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    if (!email || !password) {
+      alert("Por favor, completa todos los campos.");
+      e.preventDefault();
+    }
+  });
+  
